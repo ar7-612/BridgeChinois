e369fca7ea37f3259cf16a7e9f1a7195dbde5be9
BelhouchetSamy <95877444+BelhouchetSamy@users.noreply.github.com>
2022-06-01 00:17:03 +0200

    Add files via upload

---
+++ Sources/Modele/Historique.java
@@ -0,0 +1,55 @@
+package Modele;
+
+import java.util.Stack;
+import java.io.Serializable;
+import java.util.Enumeration;
+
+public class Historique<E extends Commande> implements Serializable{
+	Stack<E> passe, futur;
+
+    Historique() {
+        passe = new Stack<>();
+        futur = new Stack<>();
+    }
+    
+    public Enumeration<E> getEnumPasse () {
+    	return passe.elements();
+    }
+
+    void nouveau(E c) {
+        passe.push(c);
+        c.Execute();
+        while (!futur.isEmpty())
+            futur.pop();
+    }
+
+    public boolean peutAnnuler() {
+        return !passe.isEmpty();
+    }
+
+    E annuler() {
+        if (peutAnnuler()) {
+            E c = passe.pop();
+            c.DesExecute();
+            futur.push(c);
+            return c;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean peutRefaire() {
+        return !futur.isEmpty();
+    }
+
+    E refaire() {
+        if (peutRefaire()) {
+            E c = futur.pop();
+            c.Execute();
+            passe.push(c);
+            return c;
+        } else {
+            return null;
+        }
+    }
+}
